62ecd9f3fb29811c0bd2605a411c28d76a6baf3a
Ronal Castellon <ronald.castellon@jalasoft.com>
2018-04-10 14:38:07 -0400

    added advances search controls

--- src/com/jalasoft/search/view/View.java
+++ src/com/jalasoft/search/view/View.java
@@ -22,9 +22,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Calendar;
+
 
 public class View extends JFrame {
-
     private JButton btSearch;
     private JButton btCancel;
     private JButton btSelect;
@@ -40,7 +41,6 @@ public class View extends JFrame {
     private JCheckBox ckBxAdvancedSearch;
     private JLabel lbContains;
     private JFormattedTextField tBxContains;
-    private JLabel lbInTitle;
     private JCheckBox chBxInTitle;
     private JLabel lbInsideFile;
     private JCheckBox ckBxInsideFile;
@@ -58,45 +58,52 @@ public class View extends JFrame {
     private JCheckBox chBxAccessed;
     private JLabel lbAccessed;
     private JCheckBox chBxHiddenFiles;
-    private JLabel lbHiddenFiles;
     private JCheckBox ckBxHiddenFilesOnly;
     private JLabel lbHiddenFilesOnly;
     private JFrame mainFrame;
     private JPanel searchPanel;
+    private JPanel basicSearchPanel;
+    private JPanel advSearchPanel;
     private JPanel resultPanel;
     private GridBagConstraints gridBagConstraints;
     private JTable table;
     private DefaultTableModel tableModel;
     private JScrollPane scrollPane;
     public String[][] data;
-    private JPanel basicSearchPanel;
-    static final String EMPTY = "                                 ";
+    String[] columns = {"File", "Ext", "Size", "Path", "Owner"};
+    private JDialog dialogBox;
 
     /**
-     * this method creates all the UI components
+     * this method creates all the UI componens
      */
     public void createWindowObjects() {
         btSearch = new JButton("Search");
         btCancel = new JButton("Close");
         btSelect = new JButton("Find..");
         cBxAdvancedSearch = new JComboBox(advancedSearch);
+        cBxAdvancedSearch.setEnabled(false);
         ckBxAdvancedSearch = new JCheckBox("Advanced Search");
         lbFileName = new JLabel("File name:");
         lbFolder = new JLabel("Path: ");
-        tBxSearch = new JFormattedTextField(EMPTY);
-        tBxSearchPath = new JFormattedTextField(EMPTY);
+        tBxSearch = new JFormattedTextField();
+        tBxSearch.setPreferredSize(new Dimension(140,25));
+        tBxSearchPath = new JFormattedTextField();
+        Font font = new Font("TimesRoman",Font.ITALIC,16);
+        Dimension dimension = new Dimension(400,30);
+        tBxSearchPath.setMinimumSize(dimension);
+        tBxSearchPath.setFont(font);
+        tBxSearchPath.setPreferredSize(new Dimension(140, 25));
         lbContains = new JLabel("Contains:");
         lbContains.setLocation(300, 250);
         tBxContains = new JFormattedTextField();
         tBxContains.setPreferredSize(new Dimension(120, 25));
-        lbInTitle = new JLabel("In title");
-        chBxInTitle = new JCheckBox();
+        chBxInTitle = new JCheckBox("In title");
         lbInsideFile = new JLabel("Inside file");
-        ckBxInsideFile = new JCheckBox();
-        lbSize = new JLabel("File size");
+        ckBxInsideFile = new JCheckBox("Inside file");
+        lbSize = new JLabel("File size is");
         cBxSizeCriteria = new JComboBox(searchCriteria);
         tBxSize = new JFormattedTextField();
-        tBxSize.setPreferredSize(new Dimension(40, 25));
+        tBxSize.setPreferredSize(new Dimension(70, 25));
         cBxMeasureUnit = new JComboBox(measureUnit);
         lbOwner = new JLabel("Owner is:");
         tBxOwner = new JFormattedTextField();
@@ -108,60 +115,119 @@ public class View extends JFrame {
         lbModified = new JLabel("Modified");
         chBxAccessed = new JCheckBox();
         lbAccessed = new JLabel("Accessed");
-        chBxHiddenFiles = new JCheckBox();
-        lbHiddenFiles = new JLabel("Include Hidden files");
+        chBxHiddenFiles = new JCheckBox("Include hidden files");
         ckBxHiddenFilesOnly = new JCheckBox();
         lbHiddenFilesOnly = new JLabel("Hidden files only");
         mainFrame = new JFrame();
         searchPanel = new JPanel(new GridBagLayout());
+        basicSearchPanel = new JPanel(new GridBagLayout());
+        advSearchPanel = new JPanel(new GridBagLayout());
+        advSearchPanel.setVisible(false);
         gridBagConstraints = new GridBagConstraints();
         resultPanel = new JPanel();
-        basicSearchPanel = new JPanel();
+        data = new String[][]{};
+        tableModel = new DefaultTableModel(data, columns);
+        table = new JTable(tableModel);
+        scrollPane = new JScrollPane(table);
+        dialogBox = new JDialog();
     }
 
     /**
      * This method sets all objects created to its corresponding panel
      */
     public void setObjectsInWindow() {
+        //Setting components in Basic search Panel
         gridBagConstraints.insets = new Insets(2, 2, 2, 2);
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
-        searchPanel.add(lbFolder, gridBagConstraints);
+        basicSearchPanel.add(lbFolder, gridBagConstraints);
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
-        searchPanel.add(tBxSearchPath, gridBagConstraints);
+        basicSearchPanel.add(tBxSearchPath, gridBagConstraints);
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 1;
-        searchPanel.add(btSelect, gridBagConstraints);
+        basicSearchPanel.add(btSelect, gridBagConstraints);
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 2;
-        searchPanel.add(lbFileName, gridBagConstraints);
+        basicSearchPanel.add(lbFileName, gridBagConstraints);
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 2;
-        searchPanel.add(tBxSearch, gridBagConstraints);
+        basicSearchPanel.add(tBxSearch, gridBagConstraints);
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 3;
-        searchPanel.add(ckBxAdvancedSearch, gridBagConstraints);
+        basicSearchPanel.add(btSearch, gridBagConstraints);
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 3;
-        searchPanel.add(cBxAdvancedSearch, gridBagConstraints);
+        basicSearchPanel.add(btCancel, gridBagConstraints);
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 4;
-        searchPanel.add(btSearch, gridBagConstraints);
+        basicSearchPanel.add(ckBxAdvancedSearch, gridBagConstraints);
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 4;
-        searchPanel.add(btCancel, gridBagConstraints);
-        String[] columns = {"File", "Ext", "Size", "Path", "Owner"};
-        data = new String[][]{};
-        tableModel = new DefaultTableModel(data, columns);
-        table = new JTable(tableModel);
+        basicSearchPanel.add(cBxAdvancedSearch, gridBagConstraints);
+        //Setting components in the advanced Search Panel
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        advSearchPanel.add(lbContains, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        advSearchPanel.add(tBxContains, gridBagConstraints);
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 1;
+        advSearchPanel.add(chBxInTitle, gridBagConstraints);
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 1;
+        advSearchPanel.add(ckBxInsideFile, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        advSearchPanel.add(lbSize, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        advSearchPanel.add(cBxSizeCriteria, gridBagConstraints);
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 3;
+        advSearchPanel.add(tBxSize, gridBagConstraints);
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 3;
+        advSearchPanel.add(cBxMeasureUnit, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        advSearchPanel.add(lbOwner, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        advSearchPanel.add(tBxOwner, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 5;
+        advSearchPanel.add(chBxHiddenFiles, gridBagConstraints);
+        //Setting the table in the Result pane
         table.setPreferredScrollableViewportSize(new Dimension(855, 650));
         table.setFillsViewportHeight(true);
-        scrollPane = new JScrollPane(table);
         resultPanel.add(scrollPane);
-        basicSearchPanel.add(searchPanel, BorderLayout.NORTH);
-        mainFrame.add(basicSearchPanel, BorderLayout.WEST);
+        //Adding basic search and advanced search panels into Search panel
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        basicSearchPanel.setPreferredSize(new Dimension(460,160));
+        searchPanel.add(basicSearchPanel, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        advSearchPanel.setPreferredSize(new Dimension(460,160));
+        searchPanel.add(advSearchPanel, gridBagConstraints);
+        searchPanel.setBackground(Color.GRAY);
+        basicSearchPanel.setBackground(Color.lightGray);
+        advSearchPanel.setBackground(Color.lightGray);
+        resultPanel.setBackground(Color.gray);
+        //Adding the two main panels in the frame
+        mainFrame.add(searchPanel, BorderLayout.WEST);
         mainFrame.add(resultPanel, BorderLayout.CENTER);
+    }
+
+    /**
+     *
+     * This method displays a popup window with a title and a message received as parameters
+     */
+    public void showMessage(String title, String message){
+        JOptionPane.showMessageDialog(mainFrame , message,title,JOptionPane.WARNING_MESSAGE);
+
     }
 
     /**
@@ -173,13 +239,57 @@ public class View extends JFrame {
     }
 
     /**
+     * this method returns a value to indicate whether hidden files are included in the result or not
+     * @return Boolean
+     */
+    public Boolean includeHiddenFiles(){
+        return this.chBxHiddenFiles.isSelected();
+    }
+
+    /**
+     * This method returns the content of the object tBxContains, which is the advanced criteria for
+     * searching files with specific content
+     * @return String
+     */
+    public String stringContainedInSearchCriteria(){
+        return this.tBxContains.getText();
+    }
+
+    /**
+     * This method returns a value to indicate whether the string contained in the object tBxContains
+     * should be searched in the file titles or not.
+     * @return Boolean
+     */
+    public boolean searchInTitle(){
+        return this.chBxInTitle.isSelected();
+    }
+
+    /**
+     * This method returns a value to indicate whether the string contained in the object tBxContains
+     * should be searched in the content of the files  or not.
+     * @return Boolean
+     */
+    public boolean searchInsideFile(){
+        return this.ckBxInsideFile.isSelected();
+    }
+
+    /**
+     * This method returns the content of the object tBxOwner, which is the advanced criteria for
+     * searching files with an specific owner
+     * @return String
+     */
+    public String getOwnerName(){
+        return this.tBxOwner.getText();
+    }
+
+    /**
      * This method initializes the UI by creating and setting all components in the main window
      * @param windowName
      */
     public void initUI(String windowName) {
         createWindowObjects();
-        setMainWindow(windowName);
         setObjectsInWindow();
+        setMainWindow(windowName);
         enableEventsListening();
     }
 
@@ -191,8 +301,7 @@ public class View extends JFrame {
         mainFrame.setVisible(true);
         mainFrame.setSize(1250, 700);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        searchPanel.setBackground(Color.LIGHT_GRAY);
-        resultPanel.setBackground(Color.gray);
+        mainFrame.setTitle(windowTitle);
     }
 
     /**
@@ -234,9 +343,6 @@ public class View extends JFrame {
                 folder = new Folder();
                 String string = folder.getPath().toString();
                 tBxSearchPath.setText(string);
-                gridBagConstraints.gridx = 1;
-                gridBagConstraints.gridy = 1;
-                searchPanel.add(tBxSearchPath, gridBagConstraints);
             }
         });
 
@@ -274,7 +380,9 @@ public class View extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 if (cBxAdvancedSearch.getSelectedItem().toString() == "Regular files") {
                     //showAdvancedSearch();
+                    advSearchPanel.setVisible(true);
                 } else {
+                    advSearchPanel.setVisible(false);
                     //hideAdvancedSearch();
                 }
             }
